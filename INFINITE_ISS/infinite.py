83216ea34917fe9f92a2adb6e4006b53f7b543a6
semlin <valentin.aubert.45@gmail.com>
2017-05-03 20:33:20 +0200

    v0.20
    Readable output.

--- INFINITE_ISS/infinite.py
+++ INFINITE_ISS/infinite.py
@@ -4,33 +4,28 @@
 #Prérequis pip install requests
 
 import requests, os, json, datetime
+from time import gmtime, strftime
 
-#import sys, os, time
-#from datetime import datetime
-
-os.system("clear")
+#os.system("clear")
 
 def getnowAPI(arg) :
-        # Make a get request to get the latest position of the international space station from the opennotify api.
         r = requests.get("http://api.open-notify.org/iss-now.json")
-        # Print the status code of the response.
-#        print(r.status_code)
-#        print(r.text)
         paramObj = json.loads(r.text)
-#        print paramObj["arg"]
         return paramObj[arg]
 
 def getnowAPIpos(arg) :
         r = requests.get("http://api.open-notify.org/iss-now.json")
         paramObj = json.loads(r.text)
-        paramObjloc = paramObj[iss_position]
+        paramObjloc = paramObj["iss_position"]
         return paramObjloc[arg]
 
-#obj = json.loads(r.text)
-#print obj['timestamp']
+def timestampToDate(timestamps, datePattern):
+        return strftime("%b %d %Y %H:%M", gmtime(float(timestamps)))
 
-print(getnowAPI("timestamp"))
-#print('Réponse recue à {}'.format(getnowAPI("timestamp")))
+tmstmp = getnowAPI("timestamp")
+date = timestampToDate(tmstmp, "%b %d %Y %H:%M")
+
+print('\nDate et heure de la station spacial : {}\n'.format(date))
 print("La position de la station est :")
-print('Longitude : {}'.format(getnowAPIpos("longitude")))
-print('Latitude : {}'.format(getnowAPIpos("latitude")))
+print('\tLongitude : {}'.format(getnowAPIpos("longitude")))
+print('\tLatitude : {}\n'.format(getnowAPIpos("latitude")))
